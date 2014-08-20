5cd058db2263b76e41daaf0e1ac53b1184089065
Dean D. Parreno <dean105@hotmail.com>
2014-08-20 22:23:37 +1000

    layoutn 1

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,5 +10,11 @@ class StaticPagesController < ApplicationController
   
   def contact
   end
+  
+  def english
+  end
+  
+  def aspiring
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ SampleApp3::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/aspiring', to: 'static_pages#aspiring', via: 'get'
+  match '/english', to: 'static_pages#english', via: 'get'
  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
